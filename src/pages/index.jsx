18d8020d2413e95b1594e22c39cbb3d16be18142
Yoshi-0169 <pmqp1010@gmail.com>
2023-07-15 15:15:29 +0900

    🎨 クリックイベントを書いてみいた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,29 @@ import styles from 'src/styles/Home.module.css';
 import {Footer } from 'src/components/Footer';
 import {Main} from 'src/components/Main';
 import {Header}from 'src/components/Header';
+import { useCallback } from 'react';
 
 export default function Home() {
+  const of = 1;
+  const handleClick = useCallback((e)=> {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(of);
+  },[]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Super Sasaii</title>
       </Head>
-
-      <Header />
-      
+      <Header /> 
+      <a
+        href='/koko'
+        onClick={handleClick}
+        >
+        ぼたん
+      </a>
       <Main page='index!' />
-
       <Footer />
     </div>
   )
