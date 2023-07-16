40081bedf17c368e98b1c3d4de84ea082910248e
Yoshi-0169 <pmqp1010@gmail.com>
2023-07-16 20:55:06 +0900

    🎨 useEffectの追加

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,10 +2,12 @@ import styles from 'src/components/Main/Main.module.css';
 import {Headline} from 'src/components/Headline'
 import { Links } from 'src/components/Links';
 
+
 export  function Main(props) {
+
   return (
       <main className={styles.main}>
-      <Headline page={props.page}>
+         <Headline page={props.page}>
        <code className={styles.code}>pages/{props.page}</code>
       </Headline>
       <Links />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css';
 import {Footer } from 'src/components/Footer';
 import {Main} from 'src/components/Main';
 import {Header}from 'src/components/Header';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export default function Home() {
   const of = 1;
@@ -12,6 +12,17 @@ export default function Home() {
     e.preventDefault();
     alert(of);
   },[]);
+
+  useEffect(() =>{
+    console.log('マウント時');
+    document.body.style.backgroundColor = 'lightblue';
+
+    return() =>{
+    document.body.style.backgroundColor = '';
+    console.log('アンマウント時');
+    }
+
+  }, []);
 
   return (
     <div className={styles.container}>
