f2c42e4e1312b2d2c7cbd00ecc8d0e43f97e1691
Yoshi-0169 <pmqp1010@gmail.com>
2023-07-18 22:38:30 +0900

    🎨 useStateでカウンター

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,24 @@ import styles from 'src/styles/Home.module.css';
 import {Footer } from 'src/components/Footer';
 import {Main} from 'src/components/Main';
 import {Header}from 'src/components/Header';
-import { useCallback, useEffect } from 'react';
+import {  useEffect, useState } from 'react';
 
 export default function Home() {
-  const of = 1;
-  const handleClick = useCallback((e)=> {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(of);
-  },[]);
+  const [foo, setFoo] = useState(1)
+
+  const handleClick = (e)=> {
+    setFoo((foo) => foo+1);
+     setFoo((foo) => foo+1);
+    //アロー関数でかく
+
+   
+  };
 
   useEffect(() =>{
-    console.log('マウント時');
     document.body.style.backgroundColor = 'lightblue';
 
     return() =>{
     document.body.style.backgroundColor = '';
-    console.log('アンマウント時');
     }
 
   }, []);
@@ -30,12 +31,10 @@ export default function Home() {
         <title>Create Super Sasaii</title>
       </Head>
       <Header /> 
-      <a
-        href='/koko'
-        onClick={handleClick}
-        >
+      <h1>{foo}</h1>
+      <button onClick={handleClick}>
         ぼたん
-      </a>
+      </button>
       <Main page='index!' />
       <Footer />
     </div>
