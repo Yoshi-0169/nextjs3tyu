ecfe3af9ba791a85a033420da90dca9eb92472ec
Yoshi-0169 <pmqp1010@gmail.com>
2023-07-19 21:20:28 +0900

    🎨 useCallback, useEffectの第2引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,17 @@ import styles from 'src/styles/Home.module.css';
 import {Footer } from 'src/components/Footer';
 import {Main} from 'src/components/Main';
 import {Header}from 'src/components/Header';
-import {  useEffect, useState } from 'react';
+import {  useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
   const [foo, setFoo] = useState(1)
 
-  const handleClick = (e)=> {
-    setFoo((foo) => foo+1);
-     setFoo((foo) => foo+1);
+  const handleClick = useCallback(()=> {
+    if (foo < 10){
+      setFoo((foo) => foo + 1);
+    }
     //アロー関数でかく
-
-   
-  };
+  },[foo]);
 
   useEffect(() =>{
     document.body.style.backgroundColor = 'lightblue';
@@ -22,7 +21,6 @@ export default function Home() {
     return() =>{
     document.body.style.backgroundColor = '';
     }
-
   }, []);
 
   return (
