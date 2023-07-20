c8b79191021c054ea3a6693ef5e1e58758df8541
Yoshi-0169 <pmqp1010@gmail.com>
2023-07-20 10:15:08 +0900

    🎨 ぼたん表示、非表示

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,11 +6,13 @@ import {Header}from 'src/components/Header';
 import {  useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
-  const [foo, setFoo] = useState(1)
+  const [foo, setFoo] = useState(1);
+  const [text, setText] = useState('');
+  const [isShow, setIsshow] = useState(true);
 
   const handleClick = useCallback(()=> {
     if (foo < 10){
-      setFoo((foo) => foo + 1);
+      setFoo((foos) => foo + 1);
     }
     //アロー関数でかく
   },[foo]);
@@ -23,16 +25,34 @@ export default function Home() {
     }
   }, []);
 
+  const handleChange =useCallback((e) =>{
+    if(e.target.value.length > 5){
+      alert('５文字以内で');
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handlemooday = useCallback(() =>{
+            setIsshow((isShow) => !isShow)
+            //!isShowdでも
+        }, [isShow]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Super Sasaii</title>
       </Head>
       <Header /> 
-      <h1>{foo}</h1>
+      {isShow ?<h1>{foo}</h1>: null}
       <button onClick={handleClick}>
         ぼたん
       </button>
+      <button onClick={handlemooday}>{isShow ?'非表示' : '表示'}
+      </button>
+      <input type='text' value={text} onChange={handleChange}
+        
+        />
       <Main page='index!' />
       <Footer />
     </div>
